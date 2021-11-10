9df6edcc72bccff87a51c9af4258f43108f1cd05
Jiping.Liu <jiping.liu@compass.com>
2021-11-10 17:30:10 +0800

    update builder 01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,10 @@
-use proc_macro::TokenStream;
+use syn::{self, parse_macro_input};
+extern crate proc_macro;
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input_derive = parse_macro_input!(input as syn::DeriveInput);
+    eprint!("INPUT: {:#?}", input_derive.ident);
+    proc_macro::TokenStream::new()
 }
+
