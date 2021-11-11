d0ba33bc9215dd5fd5ae563c6410f2cf2bbff93e
Jiping.Liu <jiping.liu@compass.com>
2021-11-11 15:41:49 +0800

    add builder 06

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -22,12 +22,43 @@ fn get_fields_from_derive_input(d: &syn::DeriveInput) -> syn::Result<&StructFiel
     Err(syn::Error::new_spanned(d, "Must define on a Struct, not Enum".to_string()))
 }
 
+fn get_optional_inner_type(ty: &syn::Type) -> Option<&syn::Type> {
+    if let syn::Type::Path(syn::TypePath { ref path, .. }) = ty {
+        // 这里我们取segments的最后一节来判断是不是`Option<T>`，这样如果用户写的是`std:option:Option<T>`我们也能识别出最后的`Option<T>`
+        if let Some(seg) = path.segments.last() {
+            if seg.ident == "Option" {
+                if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments { 
+                    ref args, 
+                    ..
+                }) = seg.arguments
+                {
+                    if let Some(syn::GenericArgument::Type(inner_ty)) = args.first() {
+                        return Some(inner_ty);
+                    }
+                }
+            }
+        }
+    }
+    None
+}
+
 fn generate_builder_struct_fields_def(fields: &StructFields) -> syn::Result<proc_macro2::TokenStream> {
     let idents: Vec<_> = fields.iter().map(|f| { &f.ident }).collect();
-    let types: Vec<_> = fields.iter().map(|f| { &f.ty }).collect();
+    let types: Vec<_> = fields
+        .iter()
+        .map(|f| {
+            // 针对是否为 `Option` 类型字段，产生不同的结果
+            if let Some(inner_ty) = get_optional_inner_type(&f.ty) {
+                quote!(std::option::Option<#inner_ty>)
+            } else {
+                let origin_ty = &f.ty;
+                quote!(std::option::Option<#origin_ty>)
+            }
+        })
+        .collect();
 
-    let token_stream = quote!{
-        #(#idents: std::option::Option<#types>),*
+    let token_stream = quote! {
+        #(#idents: #types),*
     };
     Ok(token_stream)
 } 
@@ -50,12 +81,22 @@ fn generate_setter_funcs(fields: &StructFields) -> syn::Result<proc_macro2::Toke
     let mut final_tokenstream = proc_macro2::TokenStream::new();
 
     for (ident, ty) in idents.iter().zip(types.iter()) {
-        let tokenstream_piece = quote! {
-            fn #ident(&mut self, #ident: #ty) -> &mut Self {
-                self.#ident = std::option::Option::Some(#ident);
-                self
+        let tokenstream_piece;
+        if let Some(inner_ty) = get_optional_inner_type(ty) {
+            tokenstream_piece = quote! {
+                fn #ident(&mut self, #ident: #inner_ty) -> &mut Self {
+                    self.#ident = std::option::Option::Some(#ident);
+                    self
+                }
+            };
+        } else {
+            tokenstream_piece = quote! {
+                fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                    self.#ident = std::option::Option::Some(#ident);
+                    self
+                }
             }
-        };
+        }
         final_tokenstream.extend(tokenstream_piece);
     }
     Ok(final_tokenstream)
@@ -63,21 +104,31 @@ fn generate_setter_funcs(fields: &StructFields) -> syn::Result<proc_macro2::Toke
 
 fn generate_build_function(fields: &StructFields, origin_struct_ident: &syn::Ident) -> syn::Result<proc_macro2::TokenStream> {
     let idents: Vec<_> = fields.iter().map(|f| {&f.ident}).collect();
+    let types: Vec<_> = fields.iter().map(|f| &f.ty).collect();
 
     let mut checker_code_pieces =Vec::new();
     let mut fill_result_clauses = Vec::new();
 
-    for idx in 0..idents.len() {
-        let ident = idents[idx];
-        checker_code_pieces.push(quote! {
-            if self.#ident.is_none() {
-                let err = format!("{} field missing", stringify!(#ident));
-                return std::result::Result::Err(err.into())
-            }
-        });
-        fill_result_clauses.push(quote!{
-            #ident: self.#ident.clone().unwrap()
-        });
+    for (ident, ty) in idents.iter().zip(types.iter()) {
+        if get_optional_inner_type(ty).is_none() {
+            checker_code_pieces.push(quote! {
+                if self.#ident.is_none() {
+                    let err = format!("{} field missing", stringify!(#ident));
+                    return std::result::Result::Err(err.into())
+                }
+            });
+        }
+        
+        if get_optional_inner_type(ty).is_none() {
+            fill_result_clauses.push(quote!{
+                #ident: self.#ident.clone().unwrap()
+            });
+        } else {
+            fill_result_clauses.push(quote!{
+                #ident: self.#ident.clone()
+            });
+        }
+        
     }
 
     let token_stream = quote! {
