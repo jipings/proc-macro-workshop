14c1c3d5df621a778c33be061787efb4f379c082
Jiping.Liu <jiping.liu@compass.com>
2021-11-12 22:12:37 +0800

    add debug 01

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,18 @@
 use proc_macro::TokenStream;
+use syn::{self, spanned::Spanned};
+use quote::{ToTokens, quote};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let input_derive = syn::parse_macro_input!(input as syn::DeriveInput);
+    match do_expand(&input_derive) {
+        Ok(token_stream) => token_stream.into(),
+        Err(e) => e.to_compile_error().into(),
+    }
 }
+
+fn do_expand(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
+    let ret = proc_macro2::TokenStream::new();
+    
+    Ok(ret)
+}  
